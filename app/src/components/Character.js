a475bf89eb44f698a41ae1f022c80587ccc4a518
Sam Ardis <ardis.productions@gmail.com>
2020-03-18 16:01:09 -0400

    Added CSS styling theme GOT and messed with @media and styled-components

--- app/src/components/Character.js
+++ app/src/components/Character.js
@@ -4,6 +4,29 @@ import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner';
 
 import { fetchCharacter } from '../actions/index';
+import styled from 'styled-components';
+
+const GOTstyledDiv = styled.div`
+  background-color: #798487;
+  font-family: 'MedievalSharp', cursive;
+  font-size: 1.4rem;
+  width: 100%;
+  height: 310px;
+  padding: 1%;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  color: #34455B;
+  border: 10px solid #AD8941;
+  border-radius: 25px;
+  &:hover {
+      color: #974449;
+  }
+  @media(max-width: 400px) {
+      width: 88%;
+      margin-left: 10px;
+      font-size: 1.2rem;
+  }
+`
 
 const Character = props => {
     return (
@@ -22,12 +45,14 @@ const Character = props => {
             )}
             {props.character.map(character => {
                 return (
+                    <GOTstyledDiv>
                     <div>
                         <h2>Name: {character.name}</h2>
                         <h2>Gender: {character.gender}</h2>
                         <h2>Culture: {character.culture}</h2>
                         <h2>Alias: {character.aliases}</h2>
                     </div>
+                    </GOTstyledDiv>
                 )
             })}
         </div>
